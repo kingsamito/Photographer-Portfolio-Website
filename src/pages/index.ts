5842c236bfe591098850a6933533f372e67bfe3c
soyooye Emmanuel <soyooyeemmanuel@gmail.com>
2025-06-21 01:07:26 +0100

    feat(pages): finalize homepage and about page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
-export * from './Home'
-export * from './NotFound'
-export * from './Contact'
+export * from './Home';
+export * from './NotFound';
+export * from './Contact';
+export * from './About';
